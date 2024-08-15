f057e41f4afbb39617985ffc72c7014e6695e7e1
Jose Zudaire <62857263+JoseFZudaire@users.noreply.github.com>
2024-08-14 22:40:15 -0300

    Working on this, essentially what I have to do is work on making sure that new items are added and their quantities are correct

---
+++ Issue #030 - Revisión herramienta Lista de materiales/ListOfMaterialsPGGA/ListOfMaterialsPGGA/ListOfMaterialsPGGA/Forms/AddAdditionalShts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ListOfMaterialsPGGA.Forms
+{
+    public partial class AddAdditionalShts : Form
+    {
+        public AddAdditionalShts()
+        {
+            InitializeComponent();
+        }
+
+        string docValues = "";
+
+        public string DocValues
+        {
+            get { return docValues; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            docValues = "";
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            docValues = textBox2.Text;
+            this.Close();
+        }
+    }
+}
